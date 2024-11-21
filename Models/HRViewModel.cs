8b3052f93d146876e45d6d7ca3889fbeaa29d838
st10390381 <128587576+st10390381@users.noreply.github.com>
2024-11-21 11:31:34 +0200

    HR Model created

---
+++ Models/HRViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace PART2PROG.Models
+{
+    //HR model Created
+    public class HRViewModel
+    {
+        public int ClaimId { get; set; }
+        public string LecturerName { get; set; }
+        public decimal HoursWorked { get; set; }
+        public decimal HourlyRate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Notes { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public string Status { get; set; }
+        public bool IsApprovedByCoordinator { get; set; }
+        public bool IsApprovedByManager { get; set; }
+    }
+}
